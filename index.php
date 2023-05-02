17eaabc33d3e88a3f29e6a0752bc476ba68c0997
DLALD <luisalejandrolopezdavila@gmail.com>
2023-05-02 15:02:52 -0500

    mi primer avance

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulario de registro</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Identificacion</label>
+    <input type="text" class="form-control" id="Identificador" name="identificador">
+    
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nombre</label>
+    <input type="text" class="form-control" id="Nombre" name="Identificar">
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Detalle</label>
+    <input type="text" class="form-control" id="detalle">
+  </div>
+  <button type="submit" class="btn btn-primary">Registrar</button>
+</form>
+</body>
+</html>
